828218b0420984c5d6001ed833ec4c6c97ce587e
Jake Soenneker <jake@soenneker.com>
2025-09-15 08:34:19 -0500

    Initial commit

---
+++ src/TextDecorationStyle.cs
@@ -0,0 +1,39 @@
+﻿using Intellenum;
+using Soenneker.Quark.Enums.GlobalKeywords;
+
+namespace Soenneker.Quark.Enums.TextDecorations.Style;
+
+/// <summary>
+/// CSS text decoration keywords in enumeration form.
+/// </summary>
+[Intellenum<string>]
+public partial class TextDecorationStyle
+{
+    /// <summary>
+    /// Draws a single line segment.
+    /// This is the default style.
+    /// </summary>
+    public static readonly TextDecorationStyle Solid = new("solid");
+
+    /// <summary>
+    /// Draws two parallel lines.
+    /// </summary>
+    public static readonly TextDecorationStyle Double = new("double");
+
+    /// <summary>
+    /// Draws a dotted line.
+    /// </summary>
+    public static readonly TextDecorationStyle Dotted = new("dotted");
+
+    /// <summary>
+    /// Draws a dashed line.
+    /// </summary>
+    public static readonly TextDecorationStyle Dashed = new("dashed");
+
+    /// <summary>
+    /// Draws a wavy line.
+    /// </summary>
+    public static readonly TextDecorationStyle Wavy = new("wavy");
+
+    public static implicit operator TextDecorationStyle(GlobalKeyword style) => new(style.Value); // Value is the underlying string
+}
---
+++ test/Soenneker.Quark.Enums.TextDecorations.Style.Tests/TextDecorationStyleTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.TextDecorations.Style.Tests;
+
+public sealed class TextDecorationStyleTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
